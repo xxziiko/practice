7b0ddb4dc8e4fac44dc194e717e59f22859baa52
xxziiko <xxziiko@gmail.com>
2022-03-02 00:50:35 +0900

    Modify: Login.js, Login.scss 수정

--- webucks-react/src/pages/Login/Login.js
+++ webucks-react/src/pages/Login/Login.js
@@ -12,12 +12,12 @@ function LoginComponent() {
     <div>
       <div className="container">
         <div className="loginArea">
-          <img src="/images/WeBucks.png" alt="" />
+          <div className="title">
+            <img src="/images/WeBucks.png" alt="" />
+          </div>
           <input type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
           <input type="password" placeholder="비밀번호" />
-          <button className="btn" onClick={goToList}>
-            로그인
-          </button>
+          <button onClick={goToList}>로그인</button>
           <Link to="/signup" className="find-password" href="">
             비밀번호를 잊으셨나요?
           </Link>
