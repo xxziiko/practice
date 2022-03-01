3694b2dfd1ba4acff5e8aaecb35d7260cb83d3c3
xxziiko <xxziiko@gmail.com>
2022-03-01 17:26:41 +0900

    Add : Mission 0C - TopsNav 적용완료

---
+++ webucks-react/src/pages/Detail/Detail.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Detail.scss";
+import TopsNav from "../List/TopsNav.js";
+
+function DetailComponent() {
+  return (
+    <div>
+      <TopsNav />
+    </div>
+  );
+}
+
+export default DetailComponent;
